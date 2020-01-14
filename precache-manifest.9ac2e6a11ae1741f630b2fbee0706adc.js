67ede7e9648afa8989724364ec7d6ffbb4441fd5
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-01-14 16:53:24 -0400

    Updates

---
+++ precache-manifest.9ac2e6a11ae1741f630b2fbee0706adc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182e4d027b375c74f7c8ec9b30d107eb",
+    "url": "/resources/index.html"
+  },
+  {
+    "revision": "d87d965297659a50072a",
+    "url": "/resources/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "acaf71f0f90514fb6435",
+    "url": "/resources/static/js/2.d7e0d21c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/resources/static/js/2.d7e0d21c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d87d965297659a50072a",
+    "url": "/resources/static/js/main.69f76da1.chunk.js"
+  },
+  {
+    "revision": "9e3cdd9e374d2a54df3a",
+    "url": "/resources/static/js/runtime-main.f1dd3ad3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resources/static/media/logo.5d5d9eef.svg"
+  }
+]);
