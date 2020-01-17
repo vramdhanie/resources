562b87f10fa752c499bcaa2b575e3502ae850d69
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-01-16 22:35:03 -0400

    xSized avatar icon and used base url for fetching the icons

--- viewer/src/cardHeader.js
+++ viewer/src/cardHeader.js
@@ -5,11 +5,14 @@ import bat from "./images/bat.png";
 import styled from "styled-components";
 
 const CardHeader = ({ className, title, url }) => {
+	const urlParts = url.split("/");
+	const baseUrl = `${urlParts[0]}//${urlParts[2]}`;
+
 	return (
 		<div className={className}>
 			<div className="avatar">
 				<img
-					src={url + "/favicon.ico"}
+					src={baseUrl + "/favicon.ico"}
 					alt={title + " icon"}
 					className="icon"
 					onError={e => (e.target.src = bat)}
@@ -34,5 +37,9 @@ export default styled(CardHeader)`
 		font-size: 1.1rem;
 		font-weight: 400;
 		padding-left: 10px;
+	}
+
+	.icon {
+		width: 32px;
 	}
 `;
