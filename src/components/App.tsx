00eac5659460e7c2f82b0af9eef4730733232195
Vincent Ramdhanie <vramdhanie@gmail.com>
2021-08-11 13:18:52 -0400

    Add an alert and spinner

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,24 @@
-import { Heading, Pane, Text, majorScale } from "evergreen-ui";
+import { Heading, Pane, Text, majorScale, Spinner, Alert } from "evergreen-ui";
 
 function App() {
   return (
     <Pane>
       <Heading marginBottom={majorScale(2)}>Bookmarks</Heading>
       <Text>Welcome to the bookmarks app</Text>
+      <Alert
+        intent="none"
+        title="Coming soon!"
+        marginBottom={32}
+        marginTop={32}
+      />
+      <Pane
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        height={400}
+      >
+        <Spinner />
+      </Pane>
     </Pane>
   );
 }
